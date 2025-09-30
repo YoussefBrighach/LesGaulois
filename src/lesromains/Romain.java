7c5f60886ea1b0cc0ce9107fa5fe65d09bf63907
YoussefBrighach <youssef.brighach-ouyachou@utoulouse.fr>
2025-09-30 18:33:20 +0200

    rajout de la classe romain

--- src/lesromains/Romain.java
+++ src/lesromains/Romain.java
@@ -1,31 +1,27 @@
-package lesgaulois;
+package lesromains;
 
-import javax.sound.midi.SysexMessage;
+public class Romain {
 
-public class Gaulois {
-	
 	private String nom;
 	private int force;
-	
 
-	public Gaulois(String nom, int force) {
+	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 	}
-	
+
 	public String getNom() {
 		return nom;
 	}
-	
+
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "\"" + texte + "\"");
-		
+
 	}
 
 	private String prendreParole() {
 		// TODO Auto-generated method stub
-		return "Le gaulois " + nom + " : ";
+		return "Le romain " + nom + " : ";
 	}
-
 
 }
